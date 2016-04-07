9edc2ecd24ddfb610ae2041c78bedb58b36b5cda
brotatotes <hao.awesome@gmail.com>
2016-04-06 20:54:24 -0500

    updated run.py

--- run.py
+++ run.py
@@ -2,6 +2,8 @@ import Tkinter as tk
 # import DC_Motor as dc
 import fakedc as dc
 
+
+
 def onKeyPress(event):
 	key = event.char
 	if key == 'w':
@@ -13,12 +15,18 @@ def onKeyPress(event):
 		pass
 	elif key == 'd':
 		pass
-	else:
-		dc.stop()
+
+def onKeyRelease(event):
+	#if timer == 0:
+	dc.stop() 
+
 
 root = tk.Tk()
 root.geometry('300x200')
 text = tk.Text(root, background='black', foreground='white', font=('Comic Sans MS', 12))
 text.pack()
+button = tk.Button(root,text="W")
+button.pack()
 root.bind('<KeyPress>', onKeyPress)
+root.bind("<KeyRelease>", onKeyRelease)
 root.mainloop()
