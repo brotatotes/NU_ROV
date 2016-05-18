b1ac80e2c727491fc761676453a06979fde07910
brotatotes <hao.awesome@gmail.com>
2016-05-18 22:31:58 +0000

    removed useless things

--- DC_Motor.py
+++ DC_Motor.py
@@ -28,36 +28,8 @@ def turnLeft(leftpin,rightpin):
 
 def turnRight(leftpin,rightpin):
 	GPIO.output(leftpin,0)
-	GPIO.outpit(rightpin,1)
+	GPIO.output(rightpin,1)
 
-def main(): 
-	forwardpin = 11                         #11 high is forward
-	backwardpin = 13                        #13 low is forward
-	
-	outpin = 7                              #PWM output pin is GPIO pin 7
-	frequency = 100                         #frequency is 100 Hz
-
-	current_speed = 0			#keeps track of the current speed(duty cycle) of the RC car
-
-	GPIO.setup(outpin,GPIO.OUT)             #sets up the pin for GPIO output
-	GPIO.setup(forwardpin,GPIO.OUT)
-	GPIO.setup(backwardpin,GPIO.OUT)
-
-	GPIO.output(forwardpin, 1)              #sets pin to high
-        GPIO.output(backwardpin,0)
-
-	p=GPIO.PWM(outpin,frequency)            #configures the pin for pwm output
-        p.start(0)
-        p.ChangeDutyCycle(80)                   #starts with 0% duty cycle
-	print 'i am now here'
-	try:
-                while True:
-                        time.sleep(0.02)		# add in continuous keyboard reading here, call appropriate functions
-
-        except KeyboardInterrupt:
-                p.stop()
-                GPIO.cleanup()
-                print "stopped"
 
 
 
